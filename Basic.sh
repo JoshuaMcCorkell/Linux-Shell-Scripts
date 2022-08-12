d0c76232614ef00dcd3df8820e1803948af743ad
Joshua McCorkell <99638678+JoshuaMcCorkell@users.noreply.github.com>
2022-08-12 10:29:20 +1000

    Update Basic.sh

--- Basic.sh
+++ Basic.sh
@@ -4,3 +4,10 @@ echo 'Enabling Firewall'
 sudo ufw enable
 wait
 echo 'Enabled!'
+
+echo 'Upgrading and Updating'
+sudo apt-get update
+wait
+sudo apt-get upgrade -y
+wait
+echo 'System is up to date!'
