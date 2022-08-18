64d5fbe793a4908d88e0eff10373f14719ebe10d
Joshua McCorkell <99638678+JoshuaMcCorkell@users.noreply.github.com>
2022-08-18 14:05:22 +1000

    Update Users.sh

--- Users.sh
+++ Users.sh
@@ -4,3 +4,10 @@ echo 'Linux Hardening Script (Users) - Created by J3R August 2022'
 
 echo 'List of Users:'
 sudo ls /home
+echo 'Do you want to delete a user? y/n'
+read delyn
+if [ "$delyn" == "y" ]
+then 
+  echo "Username to delete:"
+  read username
+  userdel $username
