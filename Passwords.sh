0b151b81e00be7fc5648b3363035abcadd25910f
Joshua McCorkell <99638678+JoshuaMcCorkell@users.noreply.github.com>
2022-08-18 16:04:50 +1000

    Update Passwords.sh

--- Passwords.sh
+++ Passwords.sh
@@ -2,21 +2,21 @@
 
 echo 'Linux Hardening Script - Created by J3R August 2022'
 
-echo 'Editing Password Policies"
-sudo gedit /etc/login.defs
+echo 'Editing Password Policies'
 echo 'PASS_MAX_DAYS   90'
 echo 'PASS_MIN_DAYS   10'
 echo 'PASS_WARN_AGE   7'
+sudo gedit /etc/login.defs
 
 echo 'Press Enter to continue:'
 read
+echo 'Add this to end of file: auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800'
 sudo gedit /etc/pam.d/common-auth
-echo 'Add this to end of file: `auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800`'
 
 echo 'Press Enter to continue:'
 read
+echo 'Add this to under the line [Seat:*]: allow-guest=false'
 sudo gedit /etc/lightdm/lightdm.conf
-echo 'Add this to under the line [Seat:*]: `allow-guest=false`'
 
 echo 'Press Enter to continue:'
 read
